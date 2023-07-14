d620d9f5640e1b414802c110b8360930fb5219ed
isabella-1605 <isha.sri17@gmail.com>
2023-07-14 17:32:48 +0530

    Expense Claim resolve conflicts updates

--- src/components/modules/staffModule/ExpenseClaim/ExpenseApproveTable.js
+++ src/components/modules/staffModule/ExpenseClaim/ExpenseApproveTable.js
@@ -3,59 +3,60 @@ import { getSalary } from '../../../../services/salaryService';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { getExpenseApproval } from '../../../../services/ExpenseclaimService';
 
 const ExpenseApproveTable = () => {
-    const data = [
-        {
-            sno: 1,
-            expensehead: "",
-            amount: "",
-            approveamount: "",
-            approveremark: "",
-            costcenter: ""
-        },
-        {
-            sno: 2,
-            expensehead: "",
-            amount: "",
-            approveamount: "",
-            approveremark: "",
-            costcenter: ""
-        },
-        {
-            sno: 3,
-            expensehead: "",
-            amount: "",
-            approveamount: "",
-            approveremark: "",
-            costcenter: ""
-        },
-        {
-            sno: 4,
-            expensehead: "",
-            amount: "",
-            approveamount: "",
-            approveremark: "",
-            costcenter: ""
-        },
-        {
-            sno: 5,
-            expensehead: "",
-            amount: "",
-            approveamount: "",
-            approveremark: "",
-            costcenter: ""
-        },
-        {
-            sno: 6,
-            expensehead: "",
-            amount: "",
-            approveamount: "",
-            approveremark: "",
-            costcenter: ""
-        },
+    // const data = [
+    //     {
+    //         sno: 1,
+    //         expensehead: "",
+    //         amount: "",
+    //         approveamount: "",
+    //         approveremark: "",
+    //         costcenter: ""
+    //     },
+    //     {
+    //         sno: 2,
+    //         expensehead: "",
+    //         amount: "",
+    //         approveamount: "",
+    //         approveremark: "",
+    //         costcenter: ""
+    //     },
+    //     {
+    //         sno: 3,
+    //         expensehead: "",
+    //         amount: "",
+    //         approveamount: "",
+    //         approveremark: "",
+    //         costcenter: ""
+    //     },
+    //     {
+    //         sno: 4,
+    //         expensehead: "",
+    //         amount: "",
+    //         approveamount: "",
+    //         approveremark: "",
+    //         costcenter: ""
+    //     },
+    //     {
+    //         sno: 5,
+    //         expensehead: "",
+    //         amount: "",
+    //         approveamount: "",
+    //         approveremark: "",
+    //         costcenter: ""
+    //     },
+    //     {
+    //         sno: 6,
+    //         expensehead: "",
+    //         amount: "",
+    //         approveamount: "",
+    //         approveremark: "",
+    //         costcenter: ""
+    //     },
 
-    ]
+    // ]
     const column = [{
         headerName: "S No",
         field: "sno",
@@ -65,20 +66,50 @@ const ExpenseApproveTable = () => {
         // editable: true
     },
     {
-        headerName: "Expense Head", field: "expensehead", sortable: true
+        headerName: "Employee Code", field: "empCode", sortable: true
     },
     {
-        headerName: "Amount", field: "amount", sortable: true
+        headerName: "Claim No", field: "ClaimNo", sortable: true
     },
     {
-        headerName: "Approve Amount", field: "approveamount", sortable: true
+        headerName: "Branch", field: "Branch", sortable: true
     },
     {
-        headerName: "Approve Remark", field: "approveremark", sortable: true
+        headerName: "Date", field: "Date", sortable: true
     },
     {
-        headerName: "Cost Center", field: "costcenter", sortable: true
-    }
+        headerName: "VoucherNo", field: "VoucherNo", sortable: true
+    },
+    {
+        headerName: "Narration", field: "Narration", sortable: true
+    },
+    {
+        headerName: "ExpenseStatus", field: "expenseStatus", sortable: true
+    },
+    {
+        headerName: "Claimdate", field: "claimdate", sortable: true
+    },
+    {
+        headerName: "Processdate", field: "processdate", sortable: true
+    },
+    {
+        headerName: "ApprovedBy", field: "ApprovedBy", sortable: true
+    },
+    {
+        headerName: "creditGL", field: "creditGL", sortable: true
+    },
+    {
+        headerName: "TotalAmount", field: "TotalAmount", sortable: true
+    },
+    {
+        headerName: "ChequeNo", field: "ChequeNo", sortable: true
+    },
+    {
+        headerName: "NEFTNo", field: "NEFTNo", sortable: true
+    },
+    {
+        headerName: "CostCenter", field: "CostCenter", sortable: true
+    },
     ]
 
     const defaultColDef = {
@@ -86,12 +117,20 @@ const ExpenseApproveTable = () => {
         filter: true,
         editable: true
     }
+
+    const [approve, setApprove] = useState()
+
+    useEffect(() => {
+        getExpenseApproval().then((res) =>
+        setApprove(res.data)
+        )
+    }, [])
     return (
         <div className='justify-content-center'>
 
 
             <div className="ag-theme-alpine " style={{ width: 1200, height: 300 }}>
-                <AgGridReact rowData={data} columnDefs={column} defaultColDef={defaultColDef} />
+                <AgGridReact rowData={approve} columnDefs={column} defaultColDef={defaultColDef} />
 
             </div>
             <div className='row justify-content-center mb-5'>
--- src/share/constant.js
+++ src/share/constant.js
@@ -1,4 +1,4 @@
-const JSONURL = "http://localhost:3000"
+const JSONURL = "http://localhost:4002"
 
 export const salaryURL = `${JSONURL}/SalaryStructure`
 
@@ -7,3 +7,5 @@ export const esiPfInfoURL = `${JSONURL}/ESI-PF-Info`
 export const bankInfoURL = `${JSONURL}/BankDetails`
 
 export const basicInfoURL = `${JSONURL}/emp-basic-info`
+export const expenseInfoURL = `${JSONURL}/expense-claim-detail`
+export const expenseApproveInfoURL = `${JSONURL}/expense-claim`
